fa58fe9c9810449c1d901847964dda8cb367c8e1
648Leo_python <leonelamiranda141024@gmail.com>
2026-07-28 20:00:04 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,150 @@
+﻿// Ejercicio 1
+
+// Console.WriteLine("Hello, World!");
+
+// Ejercicio 2
+
+// int numero1 = 5;
+// string numero2 = "El numero es: ";
+
+// Console.WriteLine(numero2 + numero1); 
+
+// Ejercicio 3
+
+// string nombre = "Juan";
+// string apellido = "Perez";
+
+// Console.WriteLine($"Hola, {nombre.ToUpper()} {apellido.ToUpper()}");
+
+// string nombre = "Juan";
+// string apellido = "Perez";
+
+// Console.WriteLine($"Hola, {nombre.ToLower()} {apellido.ToLower()}");
+
+
+// Ejercicio 4
+
+// int numero1 = 5;
+// int numero2 = 10;
+// var resultado = numero1 + numero2;
+// var resultado2 = numero1 - numero2;
+// var resultado3 = numero1 * numero2;
+// var resultado4 = numero1 / numero2;
+// Console.WriteLine(resultado);
+// Console.WriteLine(resultado2);
+// Console.WriteLine(resultado3);
+// Console.WriteLine(resultado4);
+
+// Ejercicio 5
+// int numero1 = 5;
+// int numero2 = 10;
+// var igualdad = numero1 == numero2;
+// var desigualdad = numero1 != numero2;
+// var mayor = numero1 > numero2;
+// var menor = numero1 < numero2;
+// Console.WriteLine($"5 y 10 son iguales: {igualdad}");
+// Console.WriteLine($"5 y 10 son desiguales: {desigualdad}");
+// Console.WriteLine($"5 es mayor que 10: {mayor}");
+// Console.WriteLine($"5 es menor que 10: {menor}");
+
+// Ejercicio 6
+// string mensaje = "Glexibeth";
+// string NewMensaje = mensaje.Replace("Glexibeth", "Leonela");
+// Console.WriteLine(NewMensaje);
+
+// Ejercicio 7
+// Console.WriteLine("Ingrese tu nombre: ");
+// string nombre = Console.ReadLine();
+
+// Ejercicio 8
+// int numero1 = 5;
+// if (numero1 > 0)
+// {
+//     Console.WriteLine("El numero es positivo");
+// }
+// else if (numero1 < 0)
+// {
+//     Console.WriteLine("El numero es negativo");
+// }
+// else
+// {
+//     Console.WriteLine("El numero es cero");
+// }
+
+// Ejercicio 9
+
+// double numero1 = 5;
+// double numero2 = 10;
+// double resultado = numero1 + numero2;
+// double resultado2 = numero1 -numero2;
+// double resultado3 = numero1 * numero2;
+// double resultado4 = numero1 / numero2;
+// Console.WriteLine($"El resultado de los ejercicios es {resultado} ");
+// Console.WriteLine($"Es {resultado2}");
+// Console.WriteLine($"Es {resultado3}");
+// Console.WriteLine($"Es {resultado4}");
+
+// Ejercicio 10
+
+// string nombre = "Glexibeth";
+// string apellido = "Miranda";
+// Console.WriteLine($"Hola, {nombre} {apellido}");
+
+
+// Ejercicio 11
+
+// int numero1 = 5;
+// double numero2 = 10;
+// double resultado = numero1 + numero2;
+// Console.WriteLine($"El resultado es: {resultado}");
+
+// Ejercicio 12 
+
+// Console.WriteLine("Ingrese un numero: ");
+// int numero = Convert.ToInt32(Console.ReadLine());
+
+// if (numero > 0)
+// {
+//     Console.WriteLine("El numero es positivo");
+// }
+// else if (numero < 0)
+// {
+//     Console.WriteLine("El numero es negativo");
+// }
+// else
+// {
+//     Console.WriteLine("El numero es cero");
+// }
+
+// Ejercicio 13
+// for (int i = 1; i <= 10; i++)
+// {
+//     Console.WriteLine(i);
+// }
+
+// Ejercicio 14
+// string Texto1 = "    Te amo ";
+// string Texto2 = "    Leonela ";
+// string trimmedText1 = Texto1.Trim();
+// string trimmedText2 = Texto2.Trim();
+// Console.WriteLine($" {trimmedText1}");
+// Console.WriteLine($" {trimmedText2}");
+
+// Ejercicio 15
+// Console.WriteLine("Ingrese un numero: ");
+// int numero = Convert.ToInt32(Console.ReadLine());
+// for (int i = 1; i <= 10; i++)
+// {
+//     int resultado = numero * i;
+//     Console.WriteLine($"{numero} x {i} = {resultado}");
+// }
+
+// // Ejercicio 16
+// string longitud = "Glexibeth";
+// int longitudCadena = longitud.Length;
+// Console.WriteLine($"La longitud de la cadena es: {longitudCadena}");
+
+// Ejercicio 17
+// string cadena = "Glexibeth";
+// string subcadena = cadena.Substring(0, 5);
+// Console.WriteLine($"La subcadena es: {subcadena}");
